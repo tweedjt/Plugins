13406a30bfcd6adde95882454d6e8dd4778196d7
tweedjt <55853954+tweedjt@users.noreply.github.com>
2020-08-26 14:12:12 -0600

    Update

--- GoodByeMobs/src/main/java/me/tweedjt/goodbyemobs/mobs/MobsMisc.java
+++ GoodByeMobs/src/main/java/me/tweedjt/goodbyemobs/mobs/MobsMisc.java
@@ -3,6 +3,7 @@ package me.tweedjt.goodbyemobs.mobs;
 import me.tweedjt.goodbyemobs.GoodbyeMobs;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
 public class MobsMisc {
 
@@ -12,7 +13,16 @@ public class MobsMisc {
 
     public static void knockback(Player player, Entity entity) {
         // knockback code here
-    }
+        int r = GoodbyeMobs.getInstance().getGoodByeMobsConfig().getRadius();
+        Vector v = new Vector(r+2, r+2, r+2);
+
+        if (isKnockback()) {
+
+            player.getNearbyEntities(r, r, r);
+            entity.setVelocity(player.getLocation().getDirection().setY(0).normalize().multiply(v));
+            }
+        }
+
 
     public static void remove(Player player, Entity entity) {
         // We don't strictly need player here, but handy if you want it for logging
